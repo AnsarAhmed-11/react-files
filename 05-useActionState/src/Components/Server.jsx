8f646030adf9cc871763d6de17bb8bc1fd3975a0
Ansar Ahmed <irshadahmed123456780@gmail.com>
2026-06-27 12:27:34 +0530

    express-connected

--- 05-useActionState/src/Components/Server.jsx
+++ 05-useActionState/src/Components/Server.jsx
@@ -9,10 +9,10 @@ const Server = () => {
             .then((res) => setMessage(res.data.message))
             .catch((err) => console.log(err));
     }, []);
-
     return (
         <div>
             <h1>{message ? message : "not work ing"}</h1>
+            <h1>Now nxt with mysql</h1>
         </div>
     );
 };
